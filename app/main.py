923071bb8ddc2494adad2991e8d850987ee94cd4
Shreyas Khandare <shreyaskhandare2004@gmail.com>
2026-03-23 21:52:28 +0530

    Added the url of frontend

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://your-frontend-url.vercel.app"
+        "https://flow-track-bice.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
