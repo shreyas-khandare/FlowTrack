e088b5608cd74bdb905140e0185de31b20bed84d
Shreyas Khandare <shreyaskhandare2004@gmail.com>
2026-03-24 12:16:05 +0530

    fix cors origin

--- app/main.py
+++ app/main.py
@@ -10,9 +10,9 @@ Base.metadata.create_all(bind=engine)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "https://flow-track-bice.vercel.app/"
-    ],
+    "http://localhost:5173",
+    "https://flow-track-bice.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
